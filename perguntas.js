6dc80e96c96ff363e235a88781550d9c7380a43c
Anally3 <126262067+Anally3@users.noreply.github.com>
2024-08-19 08:49:23 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,45 @@
+export const perguntas = [
+    {
+        enunciado: "Você vai a casa de um amigo e se depara com placas no telhado de sua casa,logo, fica curioso para saber o que são. Qual seria o seu primeiro pensamento?",
+        alternativas: [
+            
+            {
+                texto: "eu não faço idea doque seja!",
+                afirmacao: [
+                    "Ficou em duvida do que essa tecnologia pode fazer.",
+                    "Pensou na velocidade na qual a tecnologia está avançando."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "eu conheço, é algo muito bom!",
+                afirmacao: [
+                    "Quis saber como se é utilizado no dia a dia.",
+                    "Pensou que em como pode ajudar ."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao ver seu amigo, pergunta a ele sobre as placas que viu.",
+        alternativas: [
+            {
+                texto: "é um equipamento robusto cuja função é realizar a captura dos raios de sol e transformar em energia elétrica!",
+                afirmacao: [
+                    "Consegue produzir energia limpa e sustentável.",
+                    "Percebe que ajuda muito no gasto de dinheiro na conta de luz.",
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Nossa isso parece ser muito legal mesmo! Então é uma tecnologia que ajuda o meio ambiente não é?",
+                afirmacao: [
+                    "Sim! Elas ajudam na redução de poluição por fontes contaminadoras, e de gases do efeito estufa, diminuição do desmatamento e elevação do uso de recursos naturais.",
+                    
+                ],
+            },
+        ]
+    }
+];
